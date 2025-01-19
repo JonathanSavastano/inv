a32cb9aaa6f529472270ec1103cec7d4e90fadc4
Jonny <jonathan.savastano1246@gmail.com>
2025-01-19 09:36:49 -0700

    added css and inv page

--- homepage/views.py
+++ homepage/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'homepage/home.html')
+
+def inventory(request):
+    return render(request, 'homepage/inventory.html')
